0254ccbeed096e52082f8c302a4d4d339d559549
Horacio Vega Castillo <ing.h.castillo@hotmail.com>
2014-11-21 04:08:21 -0600

    - Se modifico el Main Activity

--- KeyEvents/src/com/example/keyevents/MainActivity.java
+++ KeyEvents/src/com/example/keyevents/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.keyevents;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -18,5 +21,37 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+
+		switch(keyCode){
+			case KeyEvent.KEYCODE_CAMERA:
+				Toast.makeText(this, "Obturador presionado",
+                                                        Toast.LENGTH_SHORT).show();
+				return true;
+			case KeyEvent.KEYCODE_VOLUME_UP:
+				Toast.makeText(this, "Boton de Volumen Up presionado",
+                                                        Toast.LENGTH_SHORT).show();
+				return true;
+			case KeyEvent.KEYCODE_VOLUME_DOWN:
+				Toast.makeText(this, "Boton de Volumen Down presionado",
+                                                        Toast.LENGTH_SHORT).show();
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// TODO Auto-generated method stub
+		float posX = event.getX();
+		float posY = event.getY();
+		Toast.makeText(this, "Acabas de tocar la pantalla en: " + posX + ", " + posY,
+                                         Toast.LENGTH_SHORT).show();
+		return true;
+	}
+	
+	
 
 }
