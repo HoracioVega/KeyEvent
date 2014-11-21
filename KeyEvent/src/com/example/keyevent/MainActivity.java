900d9b1a9dc0dac582a36a020da0a1c15cff99a6
Horacio Vega Castillo <ing.h.castillo@hotmail.com>
2014-11-21 04:42:21 -0600

    - Se crea el manejo de eventos.

--- KeyEvent/src/com/example/keyevent/MainActivity.java
+++ KeyEvent/src/com/example/keyevent/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.keyevent;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -16,6 +19,39 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+
+		switch(keyCode){
+			case KeyEvent.KEYCODE_CAMERA:
+				Toast.makeText(this, "Obturador presionado",
+                                                        Toast.LENGTH_SHORT).show();
+				return true;
+			case KeyEvent.KEYCODE_VOLUME_UP:
+				Toast.makeText(this, "Boton de Volumen Up presionado",
+                                                        Toast.LENGTH_SHORT).show();
+				return true;
+			case KeyEvent.KEYCODE_VOLUME_DOWN:
+				Toast.makeText(this, "Boton de Volumen Down presionado",
+                                                        Toast.LENGTH_SHORT).show();
+			case KeyEvent.KEYCODE_BACK:
+				Toast.makeText(this, "Boton back presionado",
+                                                        Toast.LENGTH_SHORT).show();
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// TODO Auto-generated method stub
+		float posX = event.getX();
+		float posY = event.getY();
+		Toast.makeText(this, "Acabas de tocar la pantalla en: " + posX + ", " + posY,
+                                         Toast.LENGTH_SHORT).show();
 		return true;
 	}
 
